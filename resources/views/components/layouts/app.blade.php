01f22a7d0064d74a14b925c77cfe4efa00a79d01
Mudassir Ahmad Adili <mudassiradili4u@gmail.com>
2021-10-13 03:30:52 +0100

    updates

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -29,6 +29,7 @@
                 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
                     @include('partials.toolbar', ['title' => $title])
                     @include('partials.messages')
+                    @include('components.errors')
                     @include('partials.impersonate')
                     <div class="post d-flex flex-column-fluid" id="kt_post">
                         <div id="kt_content_container" class="container-fluid">
